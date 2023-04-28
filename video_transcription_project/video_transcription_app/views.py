ef7b85f5ff83a276eff4e784b1cd5a8b5e439995
Sadaf244 <sadafimukhtar@gmail.com>
2023-04-28 23:20:09 +0530

    save transcribe data in database

--- video_transcription_project/video_transcription_app/views.py
+++ video_transcription_project/video_transcription_app/views.py
@@ -1,12 +1,7 @@
-from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponseRedirect
 from .forms import video_form
-from .models import Video
-from django.views.generic.edit  import CreateView
-from django.views.generic import ListView
 from django.conf import settings
-from django.shortcuts import render, get_object_or_404
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from .models import Video
@@ -15,14 +10,30 @@ import moviepy.editor as mp
 from django.core.files.storage import FileSystemStorage
 from moviepy.editor import VideoFileClip
 import speech_recognition as sr
+import io
+import tempfile
 # from rest_framework.response import Response
+import os
+import tempfile
+from contextlib import ExitStack
+from os.path import splitext
+
+import numpy as np
+import speech_recognition as sr
+from django.shortcuts import render
+from moviepy.editor import VideoFileClip
+
+from .forms import video_form
 class AcceptVideoURL(View):
     def get(self, request):
         form=video_form()
         return render(request, "transcribe_video.html"
         ,{"form":form
         })
+
+
     def post(self, request,id=None):
+        
         form = video_form(request.POST, request.FILES)
         if form.is_valid():
             video_file = form.cleaned_data['video_file']
@@ -30,12 +41,6 @@ class AcceptVideoURL(View):
             with open(path, 'wb+') as destination:
                 for chunk in video_file.chunks():
                     destination.write(chunk)
-
-            # Create a new Video object and save it to the database
-            video = form.save(commit=False)
-            video.video_url = path
-            video.save()
-
             # Load the video file as a clip
             video_storage = FileSystemStorage()
             video_path = video_storage.path(video_file.name)
@@ -58,10 +63,10 @@ class AcceptVideoURL(View):
 
             # Delete the audio file
             video_storage.delete(audio_filename)
-            video=Video(video_url = path)
+            #saving url and transcript data in the Model
+            video=Video(video_url = path,transcript=text)
             video.save()
             # Return the transcription result
             return render(request, "video_transcription_app/transcription_result.html", context)
-        else:
-            form = VideoForm()
+       
        
